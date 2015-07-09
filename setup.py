882c8a54be6220de111dd0fa74d62c9fe7280fde
NotSpecial <alexander@dietmueller.de>
2015-07-09 16:10:12 +0200

    Adding a simple setup.py
    Just to make it importable by amivapi

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="nethz",
+    version="1.0",
+    author="Fubu",
+    description=("Library to easily access ETH ldap"),
+    packages=find_packages()
+)
